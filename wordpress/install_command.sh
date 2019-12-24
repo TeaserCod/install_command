b423b9d27d649c466d2f1391b20aef668fdbb1cc
chatbotname <teaser1st@gmail.com>
2019-12-24 16:52:11 +0700

    this file is the note download this

---
+++ wordpress/install_command.sh
@@ -0,0 +1,31 @@
+install_command.sh 
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sudo yum install -y httpd mariadb-server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl is-enabled httpd
+sudo usermod -a -G apache ec2-user
+exit
+groups
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+sudo systemctl start mariadb
+sudo mysql_secure_installation
+sudo systemctl enable mariadb
+Y
+Y
+Y
+Y
+sudo yum install php-mbstring -y
+sudo systemctl restart httpd
+sudo systemctl restart php-fpm
+cd /var/www/html
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+mkdir phpMyAdmin && tar -xvzf phpMyAdmin-latest-all-languages.tar.gz -C phpMyAdmin --strip-components 1
+rm phpMyAdmin-latest-all-languages.tar.gz
+sudo systemctl start mariadb
+
